856fc11ec7d14bef2d1ec9d61b3fb06d23da891a
jhonpulido1990 <3825holbertonstudents.com>
2024-03-03 22:19:33 -0500

    validacion de formulario login ts

--- playTech/src/app/shared/form-login/form-login.component.ts
+++ playTech/src/app/shared/form-login/form-login.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-login',
   templateUrl: './form-login.component.html',
-  styleUrls: ['./form-login.component.css']
+  styleUrls: ['./form-login.component.css'],
 })
-export class FormLoginComponent implements OnInit {
+export class FormLoginComponent {
+  formulario: FormGroup;
 
-  constructor() { }
+  formBuilder = inject(FormBuilder);
 
-  ngOnInit() {
+  constructor() {
+    this.formulario = this.formBuilder.group({
+      User: ['',[Validators.required, Validators.maxLength(20), Validators.pattern(/^[a-zA-Z0-9]+$/)]],
+      Clave: ['', [Validators.required, Validators.maxLength(20), this.noSpaces]],
+    });
   }
 
+  noSpaces(control: AbstractControl) {
+    const spaces = /\s/;
+    if (control.value && spaces.test(control.value)) {
+      return { noSpaces: true };
+    }
+    return null;
+  }
+
+  get f() {
+    return this.formulario.controls;
+  }
+
+  enviarFormulario() {
+    // Lógica para enviar o formulário
+    if (this.formulario.valid) {
+      console.log(this.formulario.value)
+    }
+  }
 }
